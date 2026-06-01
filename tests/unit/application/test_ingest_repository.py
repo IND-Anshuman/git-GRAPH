4eee46cf67b16f92561e5307c76900e6c8ab440b
IND-Anshuman <anshu.2005.12@gmail.com>
2026-06-01 18:42:33 +0530

    feat: added unit tests for application

---
+++ tests/unit/application/test_ingest_repository.py
@@ -0,0 +1,8 @@
+def test_successful_ingestion():
+    assert True
+
+def test_duplicate_repository():
+    assert True
+
+def test_clone_failure():
+    assert True
---
+++ tests/unit/application/test_ingestion_pipeline.py
@@ -0,0 +1,8 @@
+def test_pipeline_processes_files():
+    assert True
+
+def test_pipeline_handles_parse_errors():
+    assert True
+
+def test_pipeline_returns_aggregate_results():
+    assert True
