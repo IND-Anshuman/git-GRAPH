a7305bee1566e7a6924488bdd601853863f153f4
IND-Anshuman <anshu.2005.12@gmail.com>
2026-06-07 17:41:28 +0530

    feat: added drift caching benchmarks

---
+++ tests/benchmarks/concepts/caching_drift/v1_in_memory_cache.py
@@ -0,0 +1,9 @@
+class InMemoryCache:
+    def __init__(self):
+        self.store = {}
+
+    def get(self, key):
+        return self.store.get(key)
+
+    def set(self, key, value):
+        self.store[key] = value
