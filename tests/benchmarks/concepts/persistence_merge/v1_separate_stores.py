a41687c78b46e605f128bc2c646dfa5ea73a878b
IND-Anshuman <anshu.2005.12@gmail.com>
2026-06-07 17:43:10 +0530

    feat: added persistance_merge benchmarks

---
+++ tests/benchmarks/concepts/persistence_merge/v1_separate_stores.py
@@ -0,0 +1,9 @@
+class SQLStore:
+    def save_user(self, user_id, data):
+        # SQL database save operation
+        pass
+
+class MongoStore:
+    def save_document(self, doc_id, doc_body):
+        # Document store save operation
+        pass
---
+++ tests/benchmarks/concepts/persistence_merge/v2_merged_store.py
@@ -0,0 +1,8 @@
+class UnifiedStore:
+    def save_user(self, user_id, data):
+        # Merged SQL save operation
+        pass
+
+    def save_document(self, doc_id, doc_body):
+        # Merged document store save operation
+        pass
